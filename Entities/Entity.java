f74d6a33054f8781013212636e472d26ba1a1dfb
Alexander Rachenkov <aarachenkov@gmail.com>
2020-05-22 21:33:33 +0300

    fixes

--- Entities/Entity.java
+++ Entities/Entity.java
@@ -1,4 +1,3 @@
-
 abstract class Entity
 {
   private int id;
--- Main.java
+++ Main.java
@@ -1,15 +1,3 @@
-# Практическое задание.
-## Информационная система учебного заведения.
-
-[Ссылка на задание](https://docs.google.com/document/d/1COj5FSznQYb5yC-z6eeu6ggpT8VijWzjwsvZ0ugukN8/edit?usp=drivesdk)
-
-## Структура системы в виде диаграммы классов:
-
-![class diagramm](http://www.plantuml.com/plantuml/png/hLJRQkim47ptL-ZHmV4Nd1322GTEeFGbNo2YRIAmfTPQfPHUlhsoLXBh6lS2VR8yCrtlZARPEPG3-hwhJfrqJXoCGuqUjJNYkHBjV-akHq3Xq2imw1Xqidvmm94EJkW760BMx2Chpe1qghL1eTKgK4UQDNBfoCYRD6DayvdHLwkLSF8AQIezqYuJGHvDp-9WeYaVo8TY8SZQVoP4yJGkGgTwzAWHUhSMUQFNAWLN9eJKIY8RMczRiWhK4GTjpiB87lw4z-Q3oRLWWjKeu7DAdAucZhG1lWtoYBr5TrAfXOIIO83UBgLOJ15EbiGieuYoRUDxTfvYtxBcv58iEvsvoNS5-EbLBTGcCdmPLZat3MkvBPEy5cKbCgfA_DyXcw5lXB4MQsh_GTNvxH9Ysri54VXhRGVI27ZmidCrcvw_-uEInGpeptwhOirN5Mk-Kp7-KsDRVzzcTtEcT6zEfOvMLPO4k8CxB4GQcc6RyNHHcvUsV9XXoZwQTYvbc-p0ADztxm00)
-
-## Пример исполняемого файла:
-
-```Java
 import java.util.*;
 
 class Main {
@@ -60,4 +48,3 @@ class Main {
     lecturer.getCourses().forEach(c -> System.out.println(c.getName()));
   }
 }
-```
